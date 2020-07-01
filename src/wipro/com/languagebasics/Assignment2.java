cbce881a86c4535ad12b250199e8c9c3ecf4f1cf
ASUS <ASUS@DESKTOP-5FTP1OJ>
2020-07-01 20:07:28 +0530

    Intial commit

---
+++ src/wipro/com/languagebasics/Assignment2.java
@@ -0,0 +1,9 @@
+package wipro.com.languagebasics;
+
+public class Assignment2 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome "+args[0]);
+	}
+}
---
+++ src/wipro/com/languagebasics/Assignment3.java
@@ -0,0 +1,12 @@
+package wipro.com.languagebasics;
+
+public class Assignment3 
+{
+	public static void main(String args[])
+	{
+		int a,b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.println(a+b);
+	}
+}
